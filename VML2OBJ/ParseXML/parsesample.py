f7c6ffc012f370c5245f66590c14008f16d3faff
omyasuda <omyasuda@yahoo.com.br>
2022-05-02 18:03:01 -0300

    Update parsesample.py

--- VML2OBJ/ParseXML/parsesample.py
+++ VML2OBJ/ParseXML/parsesample.py
@@ -1,3 +1,4 @@
+# https://www.edureka.co/blog/python-xml-parser-tutorial/
 import xml.etree.ElementTree as ET
 fn = '/home/omy/Documentos/Programming/Python/ClubedeLeitura/VML2OBJ/ParseXML/Sample.xml'
 car = '/home/omy/Documentos/Programming/Python/ClubedeLeitura/VML2OBJ/car.vml'
