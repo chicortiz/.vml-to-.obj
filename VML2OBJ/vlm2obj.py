c024bb1755350d21e880805825f18b3e61ee738c
omyasuda <omyasuda@yahoo.com.br>
2022-05-09 18:15:59 -0300

    vml2obj writing .obj file
    writing the extracted obj data to a .obj file.

--- VML2OBJ/vlm2obj.py
+++ VML2OBJ/vlm2obj.py
@@ -1,28 +1,38 @@
-import xml.etree.ElementTree as ET
 car = '/home/omy/Documentos/Programming/Python/ClubedeLeitura/VML2OBJ/car.vml'
-mytree = ET.parse(car)
-myroot = mytree.getroot()
+carobj = '/home/omy/Documentos/Programming/Python/ClubedeLeitura/VML2OBJ/car.obj'
+building = '/home/omy/Documentos/Programming/Python/ClubedeLeitura/VML2OBJ/Building.vml'
+buildingobj = '/home/omy/Documentos/Programming/Python/ClubedeLeitura/VML2OBJ/Building.obj'
 
-# lista os vertices preparando a conversão
-iVertex = 0 # contator de vertices
-VertexDicio = dict()
-for vertex in myroot[2][0][0]:
-    VertexNum, VertexX, VertexY, VertexZ = vertex.text.split()
-    iVertex += 1
-    print('v ', VertexX, VertexY, VertexZ)
-    VertexDicio[VertexNum] = iVertex
+def vml2obj(fin, fout):
+    # parse vml file into a tree.
+    import xml.etree.ElementTree as ET
+    mytree = ET.parse(fin)
+    myroot = mytree.getroot()
+    
+    # prepare to write to file
+    fobj = open(fout, 'w')
 
-# lista as faces com os vertices renumerados
-for face in myroot[2][0][1]:
-    aface = face.text.split()
-    print('f', end= ' ')
-    for vertice in aface:
-        if vertice != '4294967295': # investigar estes casos!
-            print(VertexDicio[vertice], end=' ')
-    print()
+    # lista os vertices preparando a conversão
+    iVertex = 0 # contator de vertices
+    VertexDicio = dict()
+    for vertex in myroot[2][0][0]:
+        VertexNum, VertexX, VertexY, VertexZ = vertex.text.split()
+        fobj.write('v ' +  str(VertexX) + ' ' +  str(VertexY) + ' ' + str(VertexZ) + '\n')
+        iVertex += 1
+        VertexDicio[VertexNum] = iVertex
 
-# grava o arquivo obj.
+    # lista as faces com os vertices renumerados
+    for face in myroot[2][0][1]:
+        aface = face.text.split()
+        fobj.write('f ')
+        for vertice in aface:
+            if vertice != '4294967295': # investigar estes casos!
+                fobj.write(str(VertexDicio[vertice]) + ' ')
+        fobj.write('\n')
 
+    # conclui a gravação
+    fobj.close()
+    
 
 
 
